b512ad363c4fb01423fae1ac5dcb507065bc8a2c
Emre Cakir <cakirmehmete@gmail.com>
2015-12-14 22:00:19 -0500

    asd

---
+++ RBHS/scheduleModViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  scheduleModViewCell.swift
+//  RBHS
+//
+//  Created by Emre Cakir on 12/14/15.
+//  Copyright © 2015 Coding Empire. All rights reserved.
+//
+
+import UIKit
+
+class scheduleModViewCell : UITableViewCell {
+    
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    
+}
